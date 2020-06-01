6d4e0ed5b2b32b6fa19c44a2d8f71c6965e2797b
Hamza <Hamza@me.com>
2020-06-01 20:17:36 +0500

    first commit with rope slightly ready, hanging like a spring

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject eyes;
+    private Ray ray;
+    private RaycastHit hitInfo;
+    private bool findHook = false;
+    public GameObject ropeHook;
+    private bool canThrowHook = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ray = new Ray(eyes.transform.position, eyes.transform.right);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startFindingHook();
+        hookFindingRoutine();
+    }
+
+    private void hookFindingRoutine()
+    {
+        if (findHook)
+        {
+           ray = new Ray(eyes.transform.position, eyes.transform.right * 14);
+           if (Physics.Raycast(ray, out hitInfo, 100))
+           {
+                if (hitInfo.transform.tag == "Hook")
+                {
+                    findHook = false;
+                    Debug.DrawLine(ray.origin, hitInfo.transform.position, Color.yellow);
+                    ropeHook.GetComponent<RopeScript>().hookDest = hitInfo.transform.position;
+                    ropeHook.GetComponent<RopeScript>().playerPos = this.transform.position;
+                    throwHook();
+                }
+                else
+                {
+                    eyes.transform.Rotate(0f, 0f, 2f);
+                }
+           }
+           else
+           {
+               eyes.transform.Rotate(0f, 0f, 2f);
+           }
+        }
+    }
+
+    private void startFindingHook()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            findHook = true;
+        }
+    }
+
+    private void throwHook()
+    {
+        if(canThrowHook == false)
+        Instantiate(ropeHook, this.transform.position, Quaternion.identity);
+
+        canThrowHook = true;
+    }
+
+    public void littlePush()
+    {
+        this.GetComponent<Rigidbody>().AddForce(3f, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/RopeScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour
+{
+    public Vector3 hookDest;
+    public Vector3 playerPos;
+    private float speed = 0.3f;
+    private float segLength = 0.4f;
+    public GameObject node;
+    List<GameObject> nodes = new List<GameObject>();
+    private GameObject lastNode;
+
+    private LineRenderer lineRenderer;
+    private float lineWidth = 0.15f;
+
+    private GameObject player;
+    private bool playerConnected = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodes.Add(this.gameObject);
+        lastNode = this.gameObject;
+        lineRenderer = GetComponent<LineRenderer>();
+        if (player == null)
+        {
+            Debug.LogError("Player in ropeScript is null.");
+            player = GameObject.Find("Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hookThrower();
+        renderLine();
+    }
+
+    private void FixedUpdate()
+    {
+        //hookThrower();
+    }
+
+    private void hookThrower()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, hookDest, speed);
+
+        if (this.transform.position != hookDest)
+        {
+            if (Vector3.Distance(playerPos, lastNode.transform.position) > segLength)
+            {
+                createNode();
+                playerConnected = false;
+            }
+        }
+        else if (Vector3.Distance(playerPos, lastNode.transform.position) > segLength)
+        {
+            //while (Vector3.Distance(playerPos, lastNode.transform.position) > segLength)
+            //{
+            //    createNode();
+            //}
+        }
+        if (playerConnected == false)
+        {
+            playerConnected = true;
+            Debug.Log("player connected");
+            //player.GetComponent<SpringJoint>().connectedBody = lastNode.GetComponent<Rigidbody>();
+            lastNode.GetComponent<SpringJoint>().connectedBody = player.GetComponent<Rigidbody>();
+        }
+        //canSwing = true;
+    }
+
+    private void createNode()
+    {
+        Vector3 pos2Create = playerPos - lastNode.transform.position;
+        pos2Create.Normalize();
+        pos2Create *= segLength;
+        pos2Create += lastNode.transform.position;
+
+        GameObject go = (GameObject) Instantiate(node, pos2Create, Quaternion.identity);
+        go.transform.SetParent(this.transform);
+        go.GetComponent<SpringJoint>().connectedBody = lastNode.GetComponent<Rigidbody>();
+        nodes.Add(go);
+        lastNode = go;
+    }
+
+    private void renderLine()
+    {
+        lineRenderer.startWidth = lineWidth;
+        lineRenderer.endWidth = lineWidth;
+        lineRenderer.positionCount = nodes.Count;
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            lineRenderer.SetPosition(i, nodes[i].transform.position);
+        }
+    }
+
+    
+}
